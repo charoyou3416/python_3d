6c162901236fd863d0e407999516ca9aa7c9f72f
charoyou <charoyou0416@gmail.com>
2023-06-05 20:33:11 +0900

    3d_plot入力等変更

--- 3d_plot.py
+++ 3d_plot.py
@@ -2,99 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def read_atom_data(atom_filename):
-    atom_positions = []
-    atom_colors = []
-    atom_radii = []
-    atom_labels = []
+def plot_crystal_structure(atom_filename, bond_filename, show_labels=True):
+    # 軸の比を初期化
+    axis_ratio = [1.0, 1.0, 1.0]
 
-    with open(atom_filename, 'r', encoding='utf-8') as file:
-        # 軸の比を読み取る
-        axis_ratio_line = file.readline()
-        axis_ratio = [float(value) for value in axis_ratio_line.split('\t')]
+    # 原子データの読み込み
+    with open(atom_filename, 'r', encoding='utf-8') as atom_file:
+        lines = atom_file.readlines()
+        # 軸の比を取得
+        axis_ratio = [float(val) for val in lines[0].strip().split('\t')]
+        # 原子データを読み込み
+        atom_data = [line.strip().split('\t') for line in lines[1:]]
 
-        for line in file:
-            values = line.split('\t')
-            position = [float(values[0]), float(values[1]), float(values[2])]
-            color = [float(values[3]), float(values[4]), float(values[5])]
-            radius = float(values[6])
-            label = values[7].strip()
-            atom_positions.append(position)
-            atom_colors.append(color)
-            atom_radii.append(radius)
-            atom_labels.append(label)
+    # 結合データの読み込み
+    with open(bond_filename, 'r', encoding='utf-8') as bond_file:
+        bond_data = [line.strip().split('\t') for line in bond_file.readlines()]
 
-    return np.array(atom_positions), np.array(atom_colors), np.array(atom_radii), atom_labels, axis_ratio
-
-def read_bond_data(bond_filename):
-    bond_indices = []
-    bond_colors = []
-    bond_thicknesses = []
-
-    with open(bond_filename, 'r') as file:
-        for line in file:
-            values = line.split('\t')
-            index1 = int(values[0])
-            index2 = int(values[1])
-            color = [float(values[2]), float(values[3]), float(values[4])]
-            thickness = float(values[5])
-            bond_indices.append([index1, index2])
-            bond_colors.append(color)
-            bond_thicknesses.append(thickness)
-
-    return bond_indices, np.array(bond_colors), np.array(bond_thicknesses)
-
-def plot_crystal_structure(atom_positions, atom_colors, atom_radii, atom_labels, bond_indices, bond_colors, bond_thicknesses, show_labels=True, axis_ratio=None):
+    # 3Dプロットの設定
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.set_box_aspect(axis_ratio)
+    ax.axis('off')  # 軸を非表示にする
 
-    # 視点を調整
-    ax.view_init(elev=30, azim=45)
+    # 原子のプロット
+    for data in atom_data:
+        x, y, z, r, g, b, radius = map(float, data[:7])
+        label = str(data[7])  # ラベルを文字列に変換
+        ax.scatter(x, y, z, s=radius**2, c=(r, g, b))
 
-    # 原子の位置、色、半径をプロット
-    ax.scatter(atom_positions[:, 0], atom_positions[:, 1], atom_positions[:, 2],
-               c=atom_colors, s=atom_radii, alpha=1.0)  # 透明度を設定
+        if show_labels:
+            ax.text(x, y, z, label)
 
-    # 光源の方向を調整
-    ax.azim = 45
+    # 結合のプロット
+    for data in bond_data:
+        atom1, atom2, r, g, b, line_width = map(float, data[:6])
+        x = [float(atom_data[int(atom1) - 1][0]), float(atom_data[int(atom2) - 1][0])]
+        y = [float(atom_data[int(atom1) - 1][1]), float(atom_data[int(atom2) - 1][1])]
+        z = [float(atom_data[int(atom1) - 1][2]), float(atom_data[int(atom2) - 1][2])]
+        ax.plot(x, y, z, color=(r, g, b), linewidth=line_width)
 
-    # 原子ラベルを表示
-    if show_labels:
-        for i, label in enumerate(atom_labels):
-            ax.text(atom_positions[i, 0], atom_positions[i, 1], atom_positions[i, 2],
-                    label, color='k')
-
-    # 原子間の線を引く
-    for i, indices in enumerate(bond_indices):
-        atom1 = atom_positions[indices[0]]
-        atom2 = atom_positions[indices[1]]
-        color = bond_colors[i] if i < len(bond_colors) else 'k'
-        thickness = bond_thicknesses[i] if i < len(bond_thicknesses) else 1.0
-        ax.plot([atom1[0], atom2[0]], [atom1[1], atom2[1]], [atom1[2], atom2[2]], color=color, linewidth=thickness)
-
-    # 軸と軸ラベルを非表示にする
-    ax.set_axis_off()
-
-    # 軸の比を調整
-    if axis_ratio is not None:
-        ax.set_box_aspect(axis_ratio)
-
-    # 表示
+    # グラフの表示
     plt.show()
 
-# 原子データのテキストファイルと結合データのテキストファイルを指定
-atom_filename = 'atom_Y124.txt'
-bond_filename = 'bond_Y124.txt'
+# テスト用のファイルパス
+atom_filename = 'atom_data.txt'
+bond_filename = 'bond_data.txt'
 
-# テキストファイルから原子データと結合データを読み取る
-atom_positions, atom_colors, atom_radii, atom_labels, axis_ratio = read_atom_data(atom_filename)
-bond_indices, bond_colors, bond_thicknesses = read_bond_data(bond_filename)
-
-# ラベルの表示を選択
-show_labels = False
-
-# 軸の比を指定
-axis_ratio = axis_ratio
-
-# 結晶構造をプロット
-plot_crystal_structure(atom_positions, atom_colors, atom_radii, atom_labels, bond_indices, bond_colors, bond_thicknesses, show_labels, axis_ratio)
+# 原子構造のプロット
+plot_crystal_structure(atom_filename, bond_filename, show_labels=True)
